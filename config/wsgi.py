3b7a5443d08a94f142fe6e4367df32521dd174a8
astha <gothiastha077@gmail.com>
2026-09-05 03:08:52 +0530

    Use /tmp SQLite on Vercel

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,8 +9,15 @@ https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
 
 import os
 
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
+
+if os.environ.get("VERCEL"):
+    try:
+        call_command("migrate", interactive=False)
+    except Exception:
+        pass
